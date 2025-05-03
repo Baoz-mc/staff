fcf7f285c9de4633ad3307454cce033aab73fdc1
Salyyy <121370214+SalyyS1@users.noreply.github.com>
2025-05-03 15:42:53 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function goToDetail(memberId) {
-    window.location.href = `detail.html?id=${memberId}`;
-  }
-
-  document.addEventListener('DOMContentLoaded', () => {
-    console.log("Trang team đã sẵn sàng!");
-
-    // future animation hooks here
-  });
